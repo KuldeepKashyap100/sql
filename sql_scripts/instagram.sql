8cdb8754ed927d32044238c00817b64f14663ea4
Kuldeep <kuldeep.kumar@cdk.com>
2021-11-03 10:12:53 +0530

    sql completed

--- sql_scripts/instagram.sql
+++ sql_scripts/instagram.sql
@@ -128,3 +128,40 @@ source sql_scripts/instagram_db_data.sql;
 -- GROUP BY likes.user_id
 -- HAVING photos_liked = (SELECT COUNT(*) FROM photos);
 
+
+-- Preventing Self-Follows
+-- DELIMITER $$
+--     CREATE TRIGGER prevent_self_follows
+--     BEFORE INSERT ON follows FOR EACH ROW
+--     BEGIN
+--         IF NEW.follower_id = NEW.followee_id 
+--         THEN
+--             SIGNAL SQLSTATE '45000'
+--                 SET MESSAGE_TEXT = "You cannot follow yourself!";
+--         END IF;
+--     END;
+-- $$
+-- DELIMITER ;
+
+
+-- Logging Unfollows
+-- CREATE TABLE unfollows (
+--     follower_id INT NOT NULL,
+--     followee_id INT NOT NULL,
+--     created_at TIMESTAMP DEFAULT NOW(),
+--     FOREIGN KEY(follower_id) REFERENCES users(id),
+--     FOREIGN KEY(followee_id) REFERENCES users(id),
+--     PRIMARY KEY (follower_id, followee_id)
+-- );
+
+-- DELIMITER $$
+--     CREATE TRIGGER create_unfollow
+--         AFTER DELETE ON follows FOR EACH ROW 
+--     BEGIN
+--         INSERT INTO unfollows(followee_id, follower_id)
+--         VALUES (OLD.followee_id, OLD.follower_id);
+--         -- INSERT INTO unfollows
+--         -- SET follower_id = OLD.follower_id,
+--         --     followee_id = OLD.followee_id;
+--     END$$
+-- DELIMITER ;
---
+++ sql_scripts/triggers.sql
@@ -0,0 +1,4 @@
+DROP DATABASE IF EXISTS triggeres;
+CREATE TABLE triggers;
+USE triggeres;
+
